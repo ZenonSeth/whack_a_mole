9acc9006ec6d05e23ca2ca539e7a391e2fdf5cbe
Milcho Penchev <milcho.penchev@gmail.com>
2019-09-23 21:00:58 +0100

    Added better state tracking and on-hit animation

--- app/src/main/java/com/milchopenchev/com/whac_a_mole/feature/whack/WhackHoleLayout.kt
+++ app/src/main/java/com/milchopenchev/com/whac_a_mole/feature/whack/WhackHoleLayout.kt
@@ -4,6 +4,8 @@ import android.animation.AnimatorInflater
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.FrameLayout
+import androidx.core.animation.doOnEnd
+import androidx.core.animation.doOnStart
 import com.milchopenchev.com.whac_a_mole.R
 import kotlinx.android.synthetic.main.hole_layout.view.*
 
@@ -15,19 +17,46 @@ class WhackHoleLayout
     defStyleRes: Int = 0
 ) : FrameLayout(context, attributeSet, defStyleAttr, defStyleRes) {
 
-    private val animator = AnimatorInflater.loadAnimator(context, R.animator.peek_up_normal)
+    private enum class State {
+        Idle, Peek, Hit
+    }
+
+    private var currentState = State.Idle
+
+    private fun sync(block: () -> Unit) = synchronized(this) { block() }
+
+    private val peekAnimator =
+        AnimatorInflater.loadAnimator(context, R.animator.peek_up_normal).apply {
+            doOnStart { sync { currentState = State.Peek } }
+            doOnEnd { sync { currentState = State.Idle } }
+        }
+
+    private val hitAnimator =
+        AnimatorInflater.loadAnimator(context, R.animator.hit_animator).apply {
+            doOnStart { sync { currentState = State.Hit } }
+            doOnEnd { sync { currentState = State.Idle } }
+        }
 
     private var onMoleClicked: () -> Unit = {}
 
     init {
         inflate(context, R.layout.hole_layout, this)
-        animator.setTarget(this.the_mole)
-        this.the_mole.setOnClickListener { onMoleClicked() }
+        peekAnimator.setTarget(this.the_mole)
+        hitAnimator.setTarget(this.the_mole)
+        this.the_mole.setOnClickListener {
+            if (currentState == State.Peek) {
+                //peekAnimator.cancel()
+                hitAnimator.start()
+                onMoleClicked()
+            }
+        }
     }
 
 
-    fun popUp() {
-        animator.start()
+    fun popUp() = sync {
+        if (currentState == State.Idle) {
+            peekAnimator.start()
+        }
     }
 
     fun setOnMoleClickedListener(listener: (Int) -> Unit, id: Int) {
